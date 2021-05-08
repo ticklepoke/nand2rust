65f68e6bedcdc40c6fd2eaa47e07f3c1cedab777
Nigel Lee <lee.nigel@hotmail.com>
2021-05-08 15:41:16 +0800

    feat(assembler): File reader

---
+++ assembler/src/main.rs
@@ -0,0 +1,19 @@
+use std::{env, io::{BufRead, BufReader}};
+use std::fs::File;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+       panic!("Usage: ./debug/assembler <.asm filepath>"); 
+    }
+    
+    let filename = &args[1];
+
+    let f = File::open(filename).expect("File not found!");
+    let reader = BufReader::new(f);
+    
+    for line in reader.lines() {
+        let line = line.expect("Unable to read line!");
+        println!("{:?}", line);
+    }
+}
