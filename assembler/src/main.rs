98e22bb294db583a80206bdc455df2aa7566c682
Nigel Lee <lee.nigel@hotmail.com>
2021-05-08 21:19:18 +0800

    feat(assembler): Implementation without symbols

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,19 +1,106 @@
-use std::{env, io::{BufRead, BufReader}};
-use std::fs::File;
+use std::{
+    env,
+    io::{BufRead, BufReader, LineWriter},
+};
+use std::{fs::File, io::Write};
+
+use instructions::{get_comp_instr, get_dest_instr, get_jump_instr};
+
+mod instructions;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-       panic!("Usage: cargo run <.asm filepath>"); 
+        panic!("Usage: cargo run <.asm filepath>");
     }
-    
-    let filename = &args[1];
 
-    let f = File::open(filename).expect("File not found!");
-    let reader = BufReader::new(f);
-    
+    let full_file_path = &args[1];
+    let split_path = full_file_path
+        .split("/")
+        .collect::<Vec<&str>>()
+        .last()
+        .unwrap()
+        .to_string();
+
+    let filename_without_extension = split_path
+        .split(".")
+        .collect::<Vec<&str>>()
+        .first()
+        .unwrap()
+        .to_string();
+
+    let input_file = File::open(full_file_path).expect("File not found!");
+    let reader = BufReader::new(input_file);
+
+    let output_file = File::create(format!("{}.hack", filename_without_extension))
+        .expect("Unable to create output file");
+    let mut output_file = LineWriter::new(output_file);
+
     for line in reader.lines() {
-        let line = line.expect("Unable to read line!");
-        println!("{:?}", line);
+        let line = line.expect("Unable to read line!").trim().to_string();
+
+        if line.len() == 0 {
+            continue;
+        }
+
+        if &line[0..1] == "@" {
+            // A instruction
+            let a_value = line.split("//").collect::<Vec<&str>>()[0]
+                .trim()
+                .to_string();
+
+            let mut a_value = a_value.chars();
+            a_value.next();
+            let a_value = a_value.as_str().parse::<u32>().unwrap();
+
+            output_file
+                .write_fmt(format_args!("0{:0>15b}\n", a_value))
+                .expect("Unable to write line")
+        } else if &line[0..2] == "//" {
+            // comment
+            continue;
+        } else {
+            // c instruction
+            if line.contains("=") && line.contains(";") {
+                // 3 part
+                let args: Vec<&str> = line.split("=").collect();
+                let dest_bin = get_dest_instr(args[0]).expect("Dest instruction missing");
+                let args: Vec<&str> = args[1].split(";").collect();
+                let comp_bin = get_comp_instr(args[0]).expect("Comp instruction missing");
+                let jump_bin = get_jump_instr(args[1]).expect("Jump instruction missing");
+
+                output_file
+                    .write_fmt(format_args!("111{}{}{}\n", comp_bin, dest_bin, jump_bin))
+                    .expect("Unable to write line")
+            } else if line.contains("=") {
+                // dest and comp
+                let args: Vec<&str> = line.split("=").collect();
+                let dest_bin = get_dest_instr(args[0]).expect("Dest instruction missing");
+                let comp_bin = get_comp_instr(args[1]).expect("Comp instruction missing");
+
+                output_file
+                    .write_fmt(format_args!("111{}{}000\n", comp_bin, dest_bin))
+                    .expect("Unable to write line")
+            } else if line.contains(";") {
+                // comp and jump
+                let args: Vec<&str> = line.split(";").collect();
+                let comp_bin = get_comp_instr(args[0]).expect("Comp instruction missing");
+                let jump_bin = get_jump_instr(args[1]).expect("Jump instruction missing");
+
+                output_file
+                    .write_fmt(format_args!("111{}000{}\n", comp_bin, jump_bin))
+                    .expect("Unable to write line")
+            } else {
+                // comp only
+                let comp_bin = get_comp_instr(line.as_str()).expect("Comp instruction missing");
+
+                output_file
+                    .write_fmt(format_args!("111{}000000\n", comp_bin))
+                    .expect("Unable to write line");
+            }
+        }
     }
+
+    output_file.flush().expect("Unable to complete file write");
+    println!("Assembly completed");
 }
